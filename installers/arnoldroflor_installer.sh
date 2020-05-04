d981dd401b739dd1c4c3e1b2aca1825876f68084
Jesús B.L. (CPCBegin) <teamcpc@gmail.com>
2020-05-04 17:28:47 +0200

    Compiling cpcec to support all archs (p.e.: raspi) Bug-fix: Allow reinstall emulator in /opt/linuxemus

--- installers/arnoldroflor_installer.sh
+++ installers/arnoldroflor_installer.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #Arnold rofl0r Emulator
 sudo apt install git build-essential libgtk2.0-dev libsdl1.2-dev libx11-dev
-git clone https://github.com/rofl0r/arnold
+git clone https://github.com/rofl0r/arnold_rofl0r
 cd arnold
 git pull
 cd src
@@ -11,4 +11,4 @@ make
 cd ../..
 sudo mkdir -p /opt/linuxemus
 sudo chmod +rwx /opt/linuxemus
-mv arnold /opt/linuxemus/arnold
+mv -f arnold /opt/linuxemus/arnold
--- installers/cpcec_installer.sh
+++ installers/cpcec_installer.sh
@@ -1,13 +1,17 @@
 #/bin/bash
 #CPCED Emulator
-sudo apt install build-essential libsdl1.2-dev 
+sudo apt install build-essential libsdl1.2-dev gcc
 mkdir cpcec
 cd cpcec
 wget -c http://cngsoft.no-ip.org/cpcec-20200430.zip
 unzip -x cpcec-20200430.zip
 rm cpcec-20200430.zip
-chmod +x *.elf
+echo "** Compiling cpcec, please wait... **"
+gcc -O2 -xc cpcec.c -DSDL_MAIN_HANDLED -lSDL2 -ocpcec.elf
+echo "** Compiling zxsec, please wait... ***"
+gcc -O2 -xc zxsec.c -DSDL_MAIN_HANDLED -lSDL2 -ozxsec.elf
+chmod +x *.sh
 cd ..
 sudo mkdir -p /opt/linuxemus
 sudo chmod +rwx /opt/linuxemus
-mv cpcec /opt/linuxemus/
+mv -p cpcec /opt/linuxemus/
