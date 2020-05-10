bd3fe4c7a1f11f98739b31cb92a8f76488f7f92f
Jesús B.L <teamcpc@gmail.com>
2020-05-10 09:26:43 +0200

    Retro Virtual Machine: el instalador detecta la arquitectura del PC e instala la versión correcta y solo si está disponible.

--- installers/retrovirtualmachine_installer.sh
+++ installers/retrovirtualmachine_installer.sh
@@ -1,4 +1,15 @@
 #/bin/bash
 #RetroVirtualMachine
-wget -c https://retrovirtualmachine.ams3.digitaloceanspaces.com/release/beta1/linux/x64/RetroVirtualMachine.2.0.beta-1.r7.linux.x64.deb
-sudo dpkg -i RetroVirtualMachine.2.0.beta-1.r7.linux.x64.deb 
+if [ `uname -m` == "x86_64" ]; then
+    echo "Instaling RVM for 64 bits..."
+    wget -c https://retrovirtualmachine.ams3.digitaloceanspaces.com/release/beta1/linux/x64/RetroVirtualMachine.2.0.beta-1.r7.linux.x64.deb
+    sudo dpkg -i RetroVirtualMachine.2.0.beta-1.r7.linux.x64.deb
+    rm -f RetroVirtualMachine.2.0.beta-1.r7.linux.x64.deb
+elif [ `uname -m` == "i686" ]; then
+    echo "Instaling RVM for 32 bits.."
+    wget -c https://retrovirtualmachine.ams3.digitaloceanspaces.com/release/beta1/linux/x86/RetroVirtualMachine.2.0.beta-1.r7.linux.x86.deb
+    sudo dpkg -i RetroVirtualMachine.2.0.beta-1.r7.linux.x86.deb
+    rm -f RetroVirtualMachine.2.0.beta-1.r7.linux.x86.deb
+else
+    echo "ERROR: RVM doen't support this architecture by the moment."
+fi
