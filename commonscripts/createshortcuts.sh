679c2d399deafc6c4146d470ad2e14301972701a
Jesús B.L. (CPCBegin) <teamcpc@gmail.com>
2020-05-12 22:29:47 +0200

    Bug-fix Wrong icon for some emulators.

--- commonscripts/createshortcuts.sh
+++ commonscripts/createshortcuts.sh
@@ -7,12 +7,12 @@ if [ -n "$1" ]; then
     fi
     sudo cp -f "../shotcuts/usr/share/applications/$1.desktop" /usr/share/applications/
     sudo cp -f "../shotcuts/etc/xdg/menus/applications-merged/$1.menu" /etc/xdg/menus/applications-merged/
-    sudo cp -f "../shotcuts/usr/share/pixmaps/$1.png" /usr/share/pixmaps/emulators.png
+    sudo cp -f "../shotcuts/usr/share/pixmaps/$1.png" /usr/share/pixmaps/
     if [ -f "/usr/share/desktop-directories/Emulators.directory" ]; then
         echo "WARNING: Emulators category exists!"
     else
         sudo cp -f ../shotcuts/usr/share/desktop-directories/Emulators.directory /usr/share/desktop-directories/Emulators.directory
-        sudo cp -f ../shotcuts/usr/share/pixmaps/emulators.png //usr/share/pixmaps/
+        sudo cp -f ../shotcuts/usr/share/pixmaps/emulators.png /usr/share/pixmaps/
     fi
 else
     echo "ERROR: No parameters found."
