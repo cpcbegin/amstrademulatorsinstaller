26b39fa7dd5f592c865676df8c32d9cc2317cbba
CPCBegin <teamcpc@gmail.com>
2020-05-14 20:28:11 +0200

    Caprice32 stop working because now can run only in its own path.

--- installers/caprice32cp_installer.sh
+++ installers/caprice32cp_installer.sh
@@ -1,10 +1,10 @@
 #/bin/bash
-#Caprice32: https://malagaoriginal.blogspot.com/2019/01/compilar-el-nuevo-caprice32-con-soporte.html
+#Caprice32
 sudo apt install git build-essential libsdl1.2-dev libfreetype6-dev libpng-dev zlib1g-dev
 git clone https://github.com/ColinPitrat/caprice32 caprice32cp
 cd caprice32cp
 git pull
 make
-sudo make install
+#sudo make install
 cd ..
-../commonscripts/createshortcuts.sh caprice32cp
+../commonscripts/createshortcuts.sh caprice32cp /opt/linuxemus
