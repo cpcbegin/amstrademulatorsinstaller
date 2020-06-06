9745d889e23795aea326812a000df63ca00205d6
CPCBegin <teamcpc@gmail.com>
2020-06-06 21:58:31 +0200

    Added ZX Spectrum included in Ubuntu repository.

---
+++ installers/fuse_installer.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+#FUSE ZX Spectrum
+sudo apt install -y fuse-emulator-gtk fuse-emulator-utils spectrum-roms
+../commonscripts/createshortcuts.sh fuse-gtk
---
+++ installers/spectemu-x11_installer.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+#FBZX Spectrum
+sudo apt install -y spectemu-x11 spectrum-roms
+../commonscripts/createshortcuts.sh xspect
