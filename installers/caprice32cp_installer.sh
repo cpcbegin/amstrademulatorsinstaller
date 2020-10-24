2139d369cd8bd1037f28732b945aab238b1eaeb4
Jesús B.L <teamcpc@gmail.com>
2020-10-24 21:45:50 +0200

    Updated caprice32 (installer + desktop shortcut) - cpctelera (development branch) - xspect (desktop shortcut).

--- installers/caprice32cp_installer.sh
+++ installers/caprice32cp_installer.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 #Caprice32
-sudo apt install -y git build-essential libsdl1.2-dev libfreetype6-dev libpng-dev zlib1g-dev
+sudo apt install -y git build-essential libsdl1.2-dev libfreetype6-dev libpng-dev zlib1g-dev libsdl2-dev
 git clone https://github.com/ColinPitrat/caprice32 caprice32cp
 cd caprice32cp
 git pull
 make
-#sudo make install
+sudo make install
 cd ..
 ../commonscripts/createshortcuts.sh caprice32cp /opt/linuxemus
--- installers/cpctelera_installer.sh
+++ installers/cpctelera_installer.sh
@@ -1,9 +1,13 @@
 #/bin/bash
 #CPCTelera: https://malagaoriginal.blogspot.com/2016/01/instalar-cpctelera-en-ubuntu.html
-sudo apt install git build-essential bison flex libboost1.71-dev libfreeimage-dev mono-runtime
-sudo apt install wget unzip wine
+sudo apt -y install git build-essential bison flex libboost1.71-dev libfreeimage-dev mono-runtime
+sudo apt -y install wget unzip wine
 git clone https://github.com/lronaldo/cpctelera
-cd cpctelera/
+sudo mkdir -p /opt/linuxemus
+sudo mv cpctelera /opt/linuxemus
+sudo chmod a+rwx /opt/linuxemus/cpctelera
+cd /opt/linuxemus/cpctelera
+git branch development origin/development
+git checkout development
 git pull
-./setup.sh 
-cd ..
+./setup.sh
