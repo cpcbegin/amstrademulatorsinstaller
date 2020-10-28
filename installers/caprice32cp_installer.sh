623221ddb458292f61bea2af6505ba84481a7af7
nestornillo <52611892+nestornillo@users.noreply.github.com>
2020-10-28 22:12:04 +0100

    Update caprice32cp_installer.sh
    new menu

--- installers/caprice32cp_installer.sh
+++ installers/caprice32cp_installer.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 #Caprice32
 sudo apt install -y git build-essential libsdl1.2-dev libfreetype6-dev libpng-dev zlib1g-dev libsdl2-dev
+cd installers
 git clone https://github.com/ColinPitrat/caprice32 caprice32cp
 cd caprice32cp
 git pull
