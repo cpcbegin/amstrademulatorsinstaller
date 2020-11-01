e5e88d27e35f00bb2549f5c0414032a25b22bb99
nestornillo <52611892+nestornillo@users.noreply.github.com>
2020-11-01 23:23:51 +0100

    new emulator version. new script menu path

--- installers/cpcec_installer.sh
+++ installers/cpcec_installer.sh
@@ -4,13 +4,13 @@ sudo apt install -y build-essential libsdl1.2-dev gcc
 cd installers
 mkdir cpcec
 cd cpcec
-wget -c http://cngsoft.no-ip.org/cpcec-20201024.zip
-unzip -xuL cpcec-20201024.zip
-rm cpcec-20201024.zip
+wget -c http://cngsoft.no-ip.org/cpcec-20201028.zip
+unzip -xuL cpcec-20201028.zip
+rm cpcec-20201028.zip
 echo "** Compiling cpcec, please wait... **"
-gcc -O2 -xc cpcec.c -DSDL_MAIN_HANDLED -lSDL2 -ocpcec.sh
+gcc -DSDL2 -O2 -xc cpcec.c -lSDL2 -ocpcec.sh
 echo "** Compiling zxsec, please wait... ***"
-gcc -O2 -xc zxsec.c -DSDL_MAIN_HANDLED -lSDL2 -ozxsec.sh
+gcc -DSDL2 -O2 -xc zxsec.c -lSDL2 -ozxsec.sh
 chmod +x *.sh
 cd ..
 ../commonscripts/createshortcuts.sh cpcec /opt/linuxemus
