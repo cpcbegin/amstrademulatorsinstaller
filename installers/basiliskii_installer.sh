bc8cd3f0d7ff21e839aaccaf6f709ca6c959de8f
Jesús B.L <teamcpc@gmail.com>
2020-11-12 20:58:53 +0100

    Bug-fix: some shotcuts now works.

--- installers/basiliskii_installer.sh
+++ installers/basiliskii_installer.sh
@@ -1,8 +1,8 @@
 #/bin/bash
+../commonscripts/createshortcuts.sh basiliskii
 sudo apt -y install python3 git make
 git clone https://github.com/cebix/macemu
 cd macemu/BasiliskII/src/Unix
 ./autogen.sh
 make
 sudo make install
-../commonscripts/createshortcuts.sh basiliskii
--- installers/rott_installer.sh
+++ installers/rott_installer.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 echo ">>> Installing Rise of the Triad Open Engine..."
+../commonscripts/createshortcuts.sh rott-shareware
 sudo apt -y install rott
 echo ">>> Installing Rise of the Triad shareware data..."
 ../commonscripts/makeopenchangetodir.sh /usr/share/games/rott/
 #wget -c http://icculus.org/rott/share/1rott13.zip
 #sudo unzip -u 1rott13.zip -d /usr/share/games/rott/
 sudo wget -c https://github.com/mozzwald/openwrt-zipit-packages/blob/master/rott-shareware/files/huntbgin.wad?raw=true -O /usr/share/games/rott/HUNTBGIN.WAD
-../commonscripts/createshortcuts.sh rott-shareware
